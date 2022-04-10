c6b283fee865e2130954f2e897d155dfcae04869
vaibhaviDixit <vaibhavidixit511@gmail.com>
2022-04-10 12:42:18 +0530

    first

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,150 @@
+/**
+* PHP Email Form Validation - v3.2
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+// (function () {
+//   "use strict";
+
+//   let forms = document.querySelectorAll('.php-email-form');
+
+//   forms.forEach( function(e) {
+//     e.addEventListener('submit', function(event) {
+//       event.preventDefault();
+
+//       let thisForm = this;
+
+//       let action = thisForm.getAttribute('action');
+//       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      
+//       if( ! action ) {
+//         displayError(thisForm, 'The form action property is not set!')
+//         return;
+//       }
+//       thisForm.querySelector('.loading').classList.add('d-block');
+//       thisForm.querySelector('.error-message').classList.remove('d-block');
+//       thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+//       let formData = new FormData( thisForm );
+
+//       if ( recaptcha ) {
+//         if(typeof grecaptcha !== "undefined" ) {
+//           grecaptcha.ready(function() {
+//             try {
+//               grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+//               .then(token => {
+//                 formData.set('recaptcha-response', token);
+//                 php_email_form_submit(thisForm, action, formData);
+//               })
+//             } catch(error) {
+//               displayError(thisForm, error)
+//             }
+//           });
+//         } else {
+//           displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+//         }
+//       } else {
+//         php_email_form_submit(thisForm, action, formData);
+//       }
+//     });
+//   });
+
+//   function php_email_form_submit(thisForm, action, formData) {
+//     alert("submit")
+//     fetch(action, {
+//       method: 'POST',
+//       url:'mailer.php'
+//       body: formData,
+//       headers: {'X-Requested-With': 'XMLHttpRequest'}
+//     })
+//     .then(response => {
+//       console.log(response)
+//       if( response.ok ) {
+//         console.log("ok")
+//         return response.text()
+//       } else {
+//         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+//       }
+//     })
+//     .then(data => {
+//       thisForm.querySelector('.loading').classList.remove('d-block');
+//       if (data.trim() == 'OK') {
+//         thisForm.querySelector('.sent-message').classList.add('d-block');
+//         thisForm.reset(); 
+//       } else {
+//         console.log("error")
+//         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+//       }
+//     })
+//     .catch((error) => {
+//       displayError(thisForm, error);
+//     });
+//   }
+
+//   function displayError(thisForm, error) {
+//     thisForm.querySelector('.loading').classList.remove('d-block');
+//     thisForm.querySelector('.error-message').innerHTML = error;
+//     thisForm.querySelector('.error-message').classList.add('d-block');
+//   }
+
+// })();
+
+
+
+
+$(document).ready(function() {
+
+  var form = $('#sendMessageForm'),
+      email = $('#email'),
+      subject = $('#subject'),
+      message = $('#message'),
+      info = $('#info'),
+      loader = $('#loader'),
+      submit = $("#submitSendMsg");
+  
+ 
+  submit.click(function(e) {
+    e.preventDefault();
+    if(validate()) {
+
+      info.html('Loading...').css('color', 'red').slideDown();
+
+      $.ajax({
+        type: "POST",
+        url: "mailer.php",
+        data: form.serialize(),
+        success:function(data) {
+         response=jQuery.parseJSON(data);
+        if(response.success) {
+          form[0].reset();
+          info.html('Message sent!').css('color', 'green').slideDown();
+        } else {
+          info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
+        }
+      }
+
+      });
+    }
+  });
+  
+  function validate() {
+    var valid = true;
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    
+    if(!regex.test(email.val())) {
+      email.css('border-color', 'red');
+      valid = false;
+    }
+    if($.trim(subject.val()) === "") {
+      subject.css('border-color', 'red');
+      valid = false;
+    }
+    if($.trim(message.val()) === "") {
+      message.css('border-color', 'red');
+      valid = false;
+    }
+    
+    return valid;
+  }
+
+});
